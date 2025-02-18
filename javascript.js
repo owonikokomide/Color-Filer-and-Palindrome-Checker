1334c0f3fb7dab38f665939d69bd21fa7ec709e3
Owonikoko Ayomide <owonikokoayomide@gmail.com>
2025-02-18 01:02:05 +0100

    palindrome added

--- javascript.js
+++ javascript.js
@@ -12,3 +12,20 @@ function randomColor(){
     const color = `rgb(${red}, ${green}, ${blue})`;
     body.style.backgroundColor = color;
 }
+
+const input = document.getElementById("input");
+
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+
+function check(){
+    const value = input.value;
+    const reverse = reverseString(value);
+    
+    if(value === reverse){
+        alert("P A L I N D R O M E")
+    }else{
+        alert("Not today!!!")
+    }
+}
